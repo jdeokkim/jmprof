e0567d5ca9db5271fd8a7bf301068adc63bbef5e
jdeokkim <jdeokkim@protonmail.com>
2024-07-15 22:27:50 +0900

    Re-configure the execution order of `jmprof-ip`

--- include/jmprof.h
+++ include/jmprof.h
@@ -40,7 +40,7 @@
 /* clang-format off */
 
 #define JMPROF_AUTHOR        "Jaedeok Kim (jdeokkim@protonmail.com)"
-#define JMPROF_VERSION       "0.0.3"
+#define JMPROF_VERSION       "0.0.4"
 
 /* ========================================================================> */
 
